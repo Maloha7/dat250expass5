07fd6bef562662f87601494fb8f00f2688af9a30
Marius Hatland <marius.hatland@student.uib.no>
2023-09-26 13:04:31 +0200

    finished experiment 3

--- demo/build.gradle.kts
+++ demo/build.gradle.kts
@@ -19,6 +19,8 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-devtools")
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 }
 
 tasks.withType<Test> {
